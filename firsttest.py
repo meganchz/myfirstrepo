39a1e61cb6addc75b9a53dc6dc55d1c04463561b
meganchz <chavescmg@gmail.com>
2024-09-08 13:43:45 -0600

    cambios

--- firsttest.py
+++ firsttest.py
@@ -1,1 +1,6 @@
 print("first test with github")
+#flujo git:
+#Paso 1: Revisar que cambios hay en local (visual studio)git status
+#Paso 2: Agregar cambios a staging (etapa del proceso en donde )git add
+#Paso 3: git commit -m "(pones un mensaje)"
+#Paso 4: git push (agrega lo del local al repositorio)
